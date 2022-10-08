5c0d28082ef59c3ff4f62424dd38bc725c3ce53e
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2022-10-08 16:14:06 +0800

    roads
    road border, lines, and following effect

--- Self_Driving_Car/car.js
+++ Self_Driving_Car/car.js
@@ -1,102 +1,82 @@
-class Car
-{
-    constructor(x,y,width,height)
-    {
-        // Store as attributes, so it remembers where and how big it is.
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+class Car {
+  constructor(x, y, width, height) {
+    // Store as attributes, so it remembers where and how big it is.
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.speed=0;
-        this.acceleration=0.2;
-        this.maxSpeed=3;
-        this.friction=0.05;
-        this.angle=0;
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
 
-        // Implementing the controls
-        this.controls=new Controls();
+    // Implementing the controls
+    this.controls = new Controls();
+  }
+
+  update() {
+    this.#move();
+  }
+
+  #move() {
+    // Controlling the car's acceleration
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
     }
 
-    update()
-    {
-        this.#move();
+    // Capping the speed
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      // (-) means  the car is going backwards
+      this.speed = -this.maxSpeed / 2;
     }
 
-    #move()
-    {
-        // Controlling the car's acceleration
-        if(this.controls.forward)
-        {
-            this.speed+=this.acceleration;
-        }
-        if(this.controls.reverse)
-        {
-            this.speed-=this.acceleration;
-        }
-
-        // Capping the speed
-        if(this.speed>this.maxSpeed)
-        {
-            this.speed=this.maxSpeed;
-        }
-        if(this.speed<-this.maxSpeed/2)
-        {
-            // (-) means  the car is going backwards
-            this.speed=-this.maxSpeed/2;
-        }
-
-        // For car's friction
-        if(this.speed>0)
-        {
-            this.speed-=this.friction;
-        }
-        if(this.speed<0)
-        {
-            this.speed+=this.friction;
-        }
-        if(Math.abs(this.speed)<this.friction)
-        {
-            this.speed=0;
-        }
-
-        if(this.speed!=0)
-        {
-            // For the reverse to go the right way.
-            const flip=this.speed>0?1:-1
-            //Controlling car's left and right.
-            if(this.controls.left)
-            {
-                this.angle+=0.03*flip;
-            }
-            if(this.controls.right)
-            {
-                this.angle-=0.03*flip;
-            }
-        }
-        // Rotating 
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
+    // For car's friction
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
     }
 
-    // For drawing a rectangle
-    draw(ctx)
-    {
-        // for rotation
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
-        ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
-        
-        // To avoid infinite translate()
-        ctx.restore();
+    if (this.speed != 0) {
+      // For the reverse to go the right way.
+      const flip = this.speed > 0 ? 1 : -1;
+      //Controlling car's left and right.
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
+    // Rotating
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  // For drawing a rectangle
+  draw(ctx) {
+    // for rotation
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+
+    // To avoid infinite translate()
+    ctx.restore();
+  }
 }
